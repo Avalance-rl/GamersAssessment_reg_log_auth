9187f5ec9ced84c80ae1b91c0580254ac3efb825
Artyom <phenix392133@mail.ru>
2024-07-12 16:06:21 +0700

    add cors

--- internal/services/router.go
+++ internal/services/router.go
@@ -4,6 +4,7 @@ import (
 	_ "dev/reglogauth/docs"
 	"dev/reglogauth/internal/handlers"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -11,7 +12,13 @@ import (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"POST", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	auth := router.Group("/api/auth")
 	{
 		auth.GET("/ping", Logger(), handlers.Ping)
